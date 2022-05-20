d7ec139a51f8abe8442448ab36d0acb5fe8a2d99
szittyu <szittyu1@gmail.com>
2022-05-20 13:37:14 +0200

    Initial Commit

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env('MY_HEROKU_URL'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: env.array('APP_KEYS'),
   },
-})
+});
