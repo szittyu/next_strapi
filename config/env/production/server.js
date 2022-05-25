e531b40145f8d5a27da95c3e4d5a7599aa4512dd
szittyu <szittyu1@gmail.com>
2022-05-25 14:10:28 +0200

    a

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -4,9 +4,7 @@ module.exports = ({ env }) => ({
     app: {
         keys: env.array('APP_KEYS')
     },
-    admin: {
-        auth: {
-            secret: env('ADMIN_JWT_SECRET',)
-        }
+    autoReload: {
+        enabled: true
     }
 })
