7fb7fd493ff0a1e13c34dfe902cb02c1fdb55146
szittyu <szittyu1@gmail.com>
2022-05-26 12:00:30 +0200

    a

--- config/database.js
+++ config/database.js
@@ -1,20 +1,11 @@
+const path = require('path');
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        host: env('DATABASE_HOST', 'learn-blog-shard-00-02.hdzq3.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'learn-blog'),
-        username: env('DATABASE_USERNAME', 'attila'),
-        password: env('DATABASE_PASSWORD', 'F5vFf2Vh2WtdYykV'),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
+    useNullAsDefault: true,
   },
 });
