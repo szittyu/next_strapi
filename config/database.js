7218e70d7b1d606ae5a71af48e736c1862a3bac0
szittyu <szittyu1@gmail.com>
2022-05-26 15:25:21 +0200

    a

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        uri: env('DATABASE_URI'),
+      },
+      options: {
+        ssl: true,
+      },
     },
-    useNullAsDefault: true,
   },
 });
